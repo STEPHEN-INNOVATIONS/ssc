d7076f953ca7059c95d5ee548d0ecb89d65163ad
STEPHEN INNOVATIONS (PVT) LTD <stepheninnovations@gmail.com>
2023-10-20 03:11:01 +0530

    “development_v2”

--- fetchData.php
+++ fetchData.php
@@ -1,7 +1,40 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+// Enable CORS (Cross-Origin Resource Sharing)
+header("Access-Control-Allow-Origin: *"); // Replace "*" with the specific origin you want to allow.
+
+// Allow the following HTTP methods (you can adjust this based on your needs)
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+
+// Allow specific headers (e.g., Content-Type, Authorization) in the request
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Allow credentials (e.g., cookies, HTTP authentication) to be included in the request
+header("Access-Control-Allow-Credentials: true");
+
+// Optionally, set the maximum age for preflight requests (in seconds)
+header("Access-Control-Max-Age: 86400"); // 24 hours
+
+// Check if this is a preflight request (OPTIONS method) and respond accordingly
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    // Return a 200 OK response without content to handle preflight requests
+    http_response_code(200);
+    exit();
+}
+
+// Your PHP code to handle the actual request goes here
+// For example, you can fetch data from a database and return it as JSON
+
+// Set the response Content-Type to JSON
+header("Content-Type: application/json");
+
+// Your response data (e.g., database query results) goes here
+$responseData = array(
+    "message" => "Data retrieved successfully",
+    "data" => array(/* Your data here */)
+);
+
+// Encode the response as JSON and output it
+echo json_encode($responseData);
 
 // Database connection parameters
 $servername = "localhost";  // Change to your MySQL server address
