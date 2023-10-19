4f0dfe6ac072fd9ec14eeae68da80492580c1da8
STEPHEN INNOVATIONS (PVT) LTD <stepheninnovations@gmail.com>
2023-10-20 03:17:10 +0530

    “development_v2”

--- submit.php
+++ submit.php
@@ -5,43 +5,45 @@ $username = "root";
 $password = "jude7733";
 $dbname = "mediDB";
 
-// Create a connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+try {
+    // Create a PDO connection
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // Set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Check if the form has been submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+    // Check if the form has been submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Retrieve form data
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-    // Hash the password using md5 (not recommended for secure password hashing)
-    $hashed_password = md5($password);
+        // Hash the password using password_hash for secure password hashing
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    // Prepare an SQL query to insert data into the 'user' table
-    $sql = "INSERT INTO user (email, password) VALUES (?, ?)";
+        // Prepare an SQL query to insert data into the 'user' table
+        $sql = "INSERT INTO user (email, password) VALUES (:email, :password)";
 
-    // Use a prepared statement to securely insert data
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $email, $hashed_password);
+        // Use a prepared statement to securely insert data
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $hashed_password);
 
-    if ($stmt->execute()) {
-        // Data has been inserted successfully
-        header("Location: index.html");
-        exit; // Terminate the script to ensure the redirection happens
-    } else {
-        // Error occurred during data insertion
-        header("Location: index.html");
-        exit; // Terminate the script to ensure the redirection happens
+        if ($stmt->execute()) {
+            // Data has been inserted successfully
+            header("Location: index.html");
+            exit; // Terminate the script to ensure the redirection happens
+        } else {
+            // Error occurred during data insertion
+            header("Location: index.html");
+            exit; // Terminate the script to ensure the redirection happens
+        }
     }
-    
-    
+} catch (PDOException $e) {
+    // Handle database connection errors
+    die("Connection failed: " . $e->getMessage());
 }
 
-// Close the database connection
-$conn->close();
+// Close the database connection (optional, as PDO automatically closes the connection at the end of the script)
+$conn = null;
 ?>
